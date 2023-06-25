49e7bf8c2f1f8fe42f9a920f96548f8c59da6c45
Екатерина Сёмина <SyominaKaterina@gmail.com>
2023-06-25 23:48:06 +0500

    Функция intersect

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//first commit}
+const array0 = [10, 3, 1]
+const array1 = [3, 2, 10, 6]
+
+function intersect(arr1, arr2) {
+    return arr1.filter(i => arr2.includes(i))
+}
+
+console.log(intersect(array0, array1))
